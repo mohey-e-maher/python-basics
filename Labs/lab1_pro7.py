f676fbb8ec0f5be76858e185c6048c53b3405d1d
Mohey Eldeen Maher <54249650+mohey-e-maher@users.noreply.github.com>
2024-02-07 23:24:25 +0200

    prob7

--- Labs/lab1_pro7.py
+++ Labs/lab1_pro7.py
@@ -4,3 +4,15 @@
 # (not an empty string/integers). then proceed to ask him for his email and
 # print all this data- (Bonus) check if it is a valid email or not
 # ------------------------------------------------------
+
+name=input("\n enter your name please: ")
+if name == "":
+    print("\n Error : you didn't input the name in right way")
+    exit()
+email = input("\n enter your Email: ")
+
+print("-----------------------------------------")
+if "gmail.com" in email:
+    print(f" your name is {name} \n your email is: {email}")
+else:
+    print("\n sorry you entered incorrect email")
